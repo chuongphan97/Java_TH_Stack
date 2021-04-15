a5db6b8076c7ded1bf91acacfac16462493ec9e6
pgtc97 <79627755+pgtc97@users.noreply.github.com>
2021-04-15 13:57:01 +0700

    update

---
+++ Stack/src/MyStack.java
@@ -0,0 +1,50 @@
+public class MyStack<E> {
+    public static int DEFAULT_CAPACITY = 10;
+    public int size=0;
+    public E[] elements;
+
+
+
+    public MyStack() {
+        elements = (E[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyStack(E[] elements) {
+        this.elements = elements;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public boolean isFull(){
+        return size == DEFAULT_CAPACITY;
+    }
+
+    public void push(E e){
+        if (isFull()){
+            System.out.println("Stack overflow");
+        } else
+        elements[size++] = e;
+
+    }
+
+    public E pop(){
+        if (isEmpty()) return null;
+        else
+        return this.elements[--size];
+    }
+
+    public int search(E e){
+        for (int i = 0; i < size; i++) {
+            if (elements[i] == e) return i;
+        }
+        return -1;
+    }
+
+    public E peek(int i){
+        return elements[i];
+    }
+
+
+}
---
+++ Stack/src/MyStackTest.java
@@ -0,0 +1,26 @@
+public class MyStackTest {
+    public static void main(String[] args) {
+        MyStack<Integer> myStack = new MyStack<>();
+        myStack.push(0);
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        myStack.push(4);
+        myStack.push(4);
+        myStack.push(4);
+        myStack.push(4);
+        myStack.push(4);
+        myStack.push(4);
+
+        myStack.push(4);
+        myStack.push(4);
+        myStack.push(4);
+
+
+        for (int i = 0; i < myStack.size; i++) {
+            System.out.println(myStack.peek(i));
+        }
+
+
+    }
+}
